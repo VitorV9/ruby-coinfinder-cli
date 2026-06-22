2340e4951f60a505dfa78acddfd913dc65e70dbd
VitorVT9 <vsilvab57@gmail.com>
2026-06-22 17:04:31 -0300

    feat: adiciona construtor na classe CoinFinder

---
+++ src/coin_finder.rb
@@ -0,0 +1,9 @@
+require 'uri' # Transforma a string do link em um objeto de rota (endereço oficial) que o Net::HTTP aceita
+require 'json' # Converte o texto bruto da API em uma Hash (dicionário) para podermos ler o preço da moeda
+require 'net/http' #Faz a viagem até a internet para buscar o texto bruto com as cotações da API
+
+class CoinFinder
+  def initialize(moeda,valor)
+    @moeda = moeda
+    @valor = valor
+  end
